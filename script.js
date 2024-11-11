66bf93c769de871c413d608ad37f4504be65ffde
sana107 <mahaksanole13@gmail.com>
2024-11-11 23:01:51 +0530

    changes

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
-
-
 const qr_img = document.getElementById("qr_img");
-
 const qr_text = document.getElementById("qr_text");
-
-
-
-const sizes = document.getElementById("sizes")
+const sizes = document.getElementById("sizes");
 const btn = document.getElementById("btn");
 
-
-
 function gen_qr() {
+  console.log("clicked");
 
-    console.log("clicked");
-
-
-    if (qr_text.value.length > 0) {
-        const Q_R = qr_text.value;
-        console.log(Q_R);
-       qr_img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${Q_R}`
-       img_box.classList.add("shown-img");
-    }
-    else{
-        qr_text.classList.add("error");
-        setTimeout(()=>{
-            qr_text.classList.remove("error")
-        },1000);
-    }
-
+  if (qr_text.value.length > 0) {
+    const Q_R = qr_text.value;
+    console.log(Q_R);
+    qr_img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${Q_R}`;
+    img_box.classList.add("shown-img");
+  } else {
+    qr_text.classList.add("error");
+    setTimeout(() => {
+      qr_text.classList.remove("error");
+    }, 1000);
+  }
 }
 
 btn.addEventListener("click", gen_qr);
-
-
